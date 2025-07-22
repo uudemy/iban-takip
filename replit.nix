7141fdd885ca0c4be5205531bd385d51247bc5a4
yusa.turan <yusa.turan@iski.gov.tr>
2025-07-22 09:52:38 +0300

    Add multi-platform support: Vercel, Netlify, and optimized Replit

--- replit.nix
+++ replit.nix
@@ -2,10 +2,6 @@
   deps = [
     pkgs.nodejs-18_x
     pkgs.nodePackages.npm
-    pkgs.nodePackages.yarn
     pkgs.nodePackages.expo-cli
   ];
-  env = {
-    PATH = "${pkgs.nodejs-18_x}/bin:${pkgs.nodePackages.npm}/bin:${pkgs.nodePackages.yarn}/bin:$PATH";
-  };
 }
